b2b33bec9e61638e3dc70bb3157f390da2eeb2d1
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2023-02-28 11:06:51 -0700

    added more AST framework

--- src/edu/westminstercollege/cmpt355/minijava/node/VarDeclarationInit.java
+++ src/edu/westminstercollege/cmpt355/minijava/node/VarDeclarationInit.java
@@ -2,9 +2,14 @@ package edu.westminstercollege.cmpt355.minijava.node;
 
 import java.util.List;
 
-public record VarDeclarationInit() implements Node {
+public record VarDeclarationInit(TypeNode type, String name, Expression value) implements Node {
     @Override
     public List<? extends Node> children() {
-        return null;
+        return List.of(value);
+    }
+
+    @Override
+    public String getNodeDescription () {
+        return String.format("VarDeclarationInit [name: %s]", name);
     }
 }
